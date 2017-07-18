55f719bc9db736d62cba2e77dbe0997cd20603ce
Jamal Rogers <jamala@gmail.com>
2017-07-18 02:42:30 -0500

    Add initial solution of p003 using sieve

---
+++ swift/p003.swift
@@ -0,0 +1,38 @@
+//
+//  p003.swift
+//  
+//
+//  Created by Jamal Rogers on 7/18/17.
+//
+
+import Foundation
+
+func primes (upTo bound: Int) -> [Int] {
+    
+    guard bound > 1 else { return [] }
+    var sieve: [Bool] = Array(repeating: true, count: bound + 1)
+    sieve[0] = false
+    sieve[1] = false
+    for number in 2...Int(sqrt(Double(bound))) {
+        if sieve[number] {
+            for multiple in stride(from: number * 2, through: bound, by: number) {
+                sieve[multiple] = false
+            }
+        }
+    }
+    return sieve.enumerated().flatMap { ( $1 ? $0 : nil ) }
+}
+
+func largestPrimeFactor(of product: Int) -> Int? {
+
+    let allPrimes = primes(upTo: product)
+
+    let primeFactors = allPrimes.filter { product % $0 == 0}
+    let largestPrimeFactor =  primeFactors.last
+
+    return largestPrimeFactor
+
+}
+
+print(largestPrimeFactor(of: 600851475143))
+
